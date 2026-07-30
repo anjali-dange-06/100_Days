2d2cdac7326e666b1d919c5b785ae885f8a5e88f
anjali-dange-06 <coderg75@gmail.com>
2026-07-30 11:30:06 +0530

    Delete src/App.java

--- src/App.java
+++
@@ -1,8 +0,0 @@
-import pattern.SingletonPattern;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println(SingletonPattern.getInstance().getValue());
-    }
-}
