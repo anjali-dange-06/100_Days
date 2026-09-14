79ba0bb772d8a34aa15297d5a4180cf33e9f3fc6
anjali-dange-06 <coderg75@gmail.com>
2026-09-14 15:06:15 +0530

    Updated linked list operations

---
+++ src/Linked_List/Length.java
@@ -0,0 +1,40 @@
+public class Length {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    Node head;
+    void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+    int length() {
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Length list = new Length();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println("Length of Linked Lisst isss: " + list.length());
+    }
+}
